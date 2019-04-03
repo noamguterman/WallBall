fcdcac262f570c410490116005980796fab95346
Andrei <andrei1292@gmail.com>
2019-04-03 22:27:38 +0300

    Back Sound dontdestroy

---
+++ Assets/Scripts/Audio/BackMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackMusic : MonoBehaviour
+{
+    public static BackMusic Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);            
+        }
+    }
+}
